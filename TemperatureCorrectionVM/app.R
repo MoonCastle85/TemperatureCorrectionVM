9590036e6881c3e4a00f6d6ea8a654d5e3ac84dc
Vanja Manborg <vanja.manborg@protonmail.com>
2024-11-01 16:43:02 +0100

    Added normalization prior to prediction

--- TemperatureCorrectionVM/app.R
+++ TemperatureCorrectionVM/app.R
@@ -8,21 +8,22 @@ library(shinythemes)
 library(shinyjs)
 library(kernlab)
 library(shinycssloaders)
+library(dlookr)
+library(performance)
 
 theme_set(ggthemes::theme_clean(base_size = 24))
 
-normal_temp <- read_csv2("https://manborgconsulting.com/Normaltemperaturer.csv", 
-                         col_types = "cnnnnnn")
+normal_temp <- read_csv2("https://manborgconsulting.com/Normaltemperaturer.csv", col_types = "cnnnnnn")
 
 # Define UI for application that draws a histogram
 my_ui <- fluidPage(
     theme = shinytheme("cerulean"),
     shinyjs::useShinyjs(),
     tags$style(HTML("
-    .btn-disabled {
-      pointer-events: none;  /* Disable click events */
-      opacity: 0.5;          /* Make the button look disabled */
-    }
+      .btn-disabled {
+        pointer-events: none;
+        opacity: 0.5;
+      }
     ")),
 
     # Application title
@@ -63,16 +64,44 @@ my_server <- function(input, output, session) {
     shinyjs::disable("download_result")
     shinyjs::addClass("download_result", "btn-disabled")
     
+    remove_outliers <- function(data, group, value) {
+      data %>%
+        group_by(!!sym(group)) %>%
+        mutate(Q1 = quantile(!!sym(value), 0.25),
+               Q3 = quantile(!!sym(value), 0.75),
+               IQR = Q3-Q1,
+               lower_bound = Q1 - 1.5 * IQR,
+               upper_bound = Q3 + 1.5 * IQR,
+               outlier = !!sym(value) < lower_bound | !!sym(value) > upper_bound) %>%
+        filter(!outlier) %>%
+        select(-Q1, -Q3, -IQR, -lower_bound, -upper_bound, -outlier)
+    }
+    
     reactive_ursprungsprofil <- reactive({
         my_file <- input$my_file
         req(my_file)
-        ursprungsprofil <- read_csv2(my_file$datapath, col_types = "nnnn", col_names = c("Hour", "Temperature", "Load"), skip = 1)
+        ursprungsprofil <- read_csv2(my_file$datapath, col_types = "nnnn", 
+                                     col_names = c("Month", "Hour", "Temperature", "Load"), skip = 1) %>%
+          mutate(Season = case_when(Month %in% c(1,2,3,12) ~ "Winter", 
+                                    Month %in% c(4,5,10,11) ~ "Spring/Autumn",
+                                    Month %in% c(6,7,8,9) ~ "Summer"))
         
         return(ursprungsprofil)
     })
     
+    reactive_profil_no_outliers <- reactive({
+      no_outliers <- reactive_ursprungsprofil() %>%
+        mutate(Temperature_bin = binning(Temperature, nbins = 20, type = "quantile")) %>%
+        remove_outliers(., "Temperature_bin", "Load")
+      
+      return(no_outliers)
+    })
+    
     reactive_selected_temp <- reactive({
-      selected_temp <- dplyr::filter(normal_temp, Ort == input$municipalities)
+      selected_temp <- dplyr::filter(normal_temp, Ort == input$municipalities) %>%
+        mutate(Season = case_when(Month %in% c(1,2,3,12) ~ "Winter", 
+                                  Month %in% c(4,5,10,11) ~ "Spring/Autumn",
+                                  Month %in% c(6,7,8,9) ~ "Summer"))
       
       return(selected_temp)
     })
@@ -82,9 +111,30 @@ my_server <- function(input, output, session) {
             set_mode("regression") %>%
             set_engine("kknn")
         
-        my_knn_fit <- my_knn %>% fit(Load ~ Temperature + Hour, data = reactive_ursprungsprofil())
-        my_knn_pred <- predict(my_knn_fit, reactive_selected_temp())
-        resultat_knn <- bind_cols(reactive_selected_temp(), Predikterat = my_knn_pred)
+        my_knn_rec <- recipe(Load ~ Season + Temperature + Hour, data = reactive_profil_no_outliers()) %>%
+          step_normalize(Temperature, id = "temp_normalize") %>%
+          step_normalize(Load, skip = TRUE, id = "load_normalize")
+        
+        my_knn_wf <- workflow() %>%
+          add_recipe(my_knn_rec) %>%
+          add_model(my_knn)
+        
+        prep_knn <- my_knn_rec %>%
+          prep()
+        
+        load_knn_mean <- tidy(prep_knn, id = "load_normalize") %>%
+          filter(terms == "Load", statistic == "mean") %>%
+          pull(value)
+        
+        load_knn_sd <- tidy(prep_knn, id = "load_normalize") %>%
+          filter(terms == "Load", statistic == "sd") %>%
+          pull(value)
+        
+        my_knn_fit <- fit(my_knn_wf, data = reactive_profil_no_outliers())
+        my_knn_pred <- predict(my_knn_fit, new_data = reactive_selected_temp())
+        my_knn_pred_unnormalized <- my_knn_pred * load_knn_sd + load_knn_mean
+        
+        resultat_knn <- bind_cols(reactive_selected_temp(), Predikterat = my_knn_pred_unnormalized)
         
         return(resultat_knn)
     })
@@ -94,9 +144,30 @@ my_server <- function(input, output, session) {
         set_mode("regression") %>%
         set_engine("kernlab")
       
-      my_svm_fit <- my_svm %>% fit(Load ~ Temperature + Hour, data = reactive_ursprungsprofil())
-      my_svm_pred <- predict(my_svm_fit, reactive_selected_temp())
-      resultat_svm <- bind_cols(reactive_selected_temp(), my_svm_pred)
+      my_svm_rec <- recipe(Load ~ Season + Temperature + Hour, data = reactive_profil_no_outliers()) %>%
+        step_normalize(Temperature, id = "temp_normalize") %>%
+        step_normalize(Load, skip = TRUE, id = "load_normalize")
+      
+      my_svm_wf <- workflow() %>%
+        add_recipe(my_svm_rec) %>%
+        add_model(my_svm)
+      
+      prep_svm <- my_svm_rec %>%
+        prep()
+      
+      load_svm_mean <- tidy(prep_svm, id = "load_normalize") %>%
+        filter(terms == "Load", statistic == "mean") %>%
+        pull(value)
+      
+      load_svm_sd <- tidy(prep_svm, id = "load_normalize") %>%
+        filter(terms == "Load", statistic == "sd") %>%
+        pull(value)
+      
+      my_svm_fit <- fit(my_svm_wf, data = reactive_profil_no_outliers())
+      my_svm_pred <- predict(my_svm_fit, new_data = reactive_selected_temp())
+      my_svm_pred_unnormalized <- my_svm_pred * load_svm_sd + load_svm_mean
+      
+      resultat_svm <- bind_cols(reactive_selected_temp(), Predikterat = my_svm_pred_unnormalized)
       
       return(resultat_svm)
     })
@@ -129,8 +200,10 @@ my_server <- function(input, output, session) {
     
     output$mainPlot <- renderPlot({
         ggplot() +
-            geom_point(data = reactive_ursprungsprofil(), mapping = aes(x = Temperature, y = Load, colour = "Original"), size = 3) +
-            geom_point(data = selected_algorithm_data(), mapping = aes(x = Temperature, y = .pred, colour = "Predicted"), size = 3) +
+            geom_point(data = reactive_ursprungsprofil(), 
+                       mapping = aes(x = Temperature, y = Load, colour = "Original"), size = 3) +
+            geom_point(data = selected_algorithm_data(), 
+                       mapping = aes(x = Temperature, y = .pred, colour = "Predicted"), size = 3) +
             labs(title = "Load vs temperature data", x = "Outdoor temperature (°C)", y = "Heating load (kW)") +
             scale_colour_manual(values = c("Original" = "blue", "Predicted" = "red"), labels = c("Original", "Predicted"),
                                 name = "Data source") +
@@ -140,10 +213,9 @@ my_server <- function(input, output, session) {
     
     output$my_DT <- DT::renderDT({
         datatable(selected_table_data(),
-                  options = list(initComplete = JS("function(settings, json) {",
-                                                   "  Shiny.setInputValue('table_rendered', true);",
-                                                   "}")
-                                 )
+                  options = list(initComplete = JS("function(settings, json) {", 
+                                                   "  Shiny.setInputValue('table_rendered', true);", "}")
+                  )
         )
     })
     
