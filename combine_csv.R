a97be99d5d42a36f53ff47855e7fea72fa3da33c
MoonCastle85 <vanja.manborg@protonmail.com>
2026-06-16 15:13:58 +0200

    Fixed the combine script so that profile_id column gets created in a correct way.

--- combine_csv.R
+++ combine_csv.R
@@ -1,9 +1,14 @@
 library(tidyverse)
 
-files <- list.files(paste0("C:/Users/vanja/OneDrive - Profu/Fjärrkontrollen - Profu - Documents/Admin/",
-                           "3. Underlag profiler, COP, inv kostnader osv/Profiler värme och el för fastigheterna/Analys profiler"),
-                    pattern = "+.csv", full.names = TRUE)
+setwd(paste0("C:/Users/vanja/OneDrive - Profu/Fjärrkontrollen - Profu - Documents/Admin/",
+             "3. Underlag profiler, COP, inv kostnader osv/Profiler värme och el för fastigheterna/Analys profiler"))
 
-combined <- map(files, read_csv2) %>% bind_rows()
+files <- list.files(".", pattern = "+.csv")
+
+combined <- map(files, \(x) read_csv2(x, col_select = -c("profile_id"), id = "profile_id")) %>%
+  bind_rows() 
+
+combined2 <- combined %>%
+  mutate(profile_id = str_extract(profile_id, ".*(?=\\.csv$)"))
 
 write_csv2(combined, file = "Profiler_alla.csv")
